37aaf9f5d3c980ed309fcb39892b610fb5f2393f
Rahul <rahul.toora@edfenergy.com>
2023-03-19 22:33:20 +0000

    adding update and delete methods

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from random import randrange
 
@@ -21,14 +21,47 @@ def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):    
     post_dict= post.dict()
     post_dict['id'] = randrange(0,100000)
     my_posts.append(post_dict)
     return {"data": post_dict}
 
+
+# @app.get("/posts/latest") # this needs to go before id one as it goes top down
+# def get_latest_posts():
+#     return {"data": my_posts[-1]}
+
 @app.get("/posts/{id}")
-async def get_posts(id: int):
-    post = next(post for post in my_posts if post['id'] == id)
+async def get_post(id: int):
+    post = next((post for post in my_posts if post['id'] == id), None)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
     return {"data": post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id: int):
+    
+    post = next((post for post in my_posts if post['id'] == id), None)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
+    my_posts.remove(post)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    
+    index = next((index for index, post in enumerate(my_posts) if post['id'] == id), None)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
+        
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
+    
+    
